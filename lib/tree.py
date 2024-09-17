3f1e947df14bd1a2a023dfe48b8b71b8ca748b55
Daniel <136662533+danielshanesilvestre@users.noreply.github.com>
2024-09-17 12:38:37 -0500

    Completed exercise

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,11 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
+    nodes_to_visit = [self.root]
+    while len(nodes_to_visit) > 0:
+      node = nodes_to_visit.pop()
+      if node['id'] == id:
+        return node
+      else:
+        nodes_to_visit = node['children'] + nodes_to_visit
     pass
